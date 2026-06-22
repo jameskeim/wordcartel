04b5eee0c53234854196b70a3da96de7eac54c75
James Keim <email@jameskeim.com>
2026-06-22 16:46:01 -0500

    chore(core): scaffold wordcartel-core crate

---
+++ wordcartel-core/target/debug/build/smartstring-30f9f3dee74676b8/build_script_build-30f9f3dee74676b8.d
@@ -0,0 +1,5 @@
+/home/jkeim/projects/groundwords/wordcartel-core/target/debug/build/smartstring-30f9f3dee74676b8/build_script_build-30f9f3dee74676b8.d: /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smartstring-1.0.1/build.rs
+
+/home/jkeim/projects/groundwords/wordcartel-core/target/debug/build/smartstring-30f9f3dee74676b8/build_script_build-30f9f3dee74676b8: /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smartstring-1.0.1/build.rs
+
+/home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smartstring-1.0.1/build.rs:
