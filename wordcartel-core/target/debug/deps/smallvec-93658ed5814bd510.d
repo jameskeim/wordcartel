0affbbc3b6e6c055a5a3ee2234abd6df4208761c
James Keim <email@jameskeim.com>
2026-06-22 16:47:40 -0500

    chore: gitignore Rust target/ dirs (untrack build artifacts)

--- wordcartel-core/target/debug/deps/smallvec-93658ed5814bd510.d
+++
@@ -1,7 +0,0 @@
-/home/jkeim/projects/groundwords/wordcartel-core/target/debug/deps/smallvec-93658ed5814bd510.d: /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.2/src/lib.rs
-
-/home/jkeim/projects/groundwords/wordcartel-core/target/debug/deps/libsmallvec-93658ed5814bd510.rlib: /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.2/src/lib.rs
-
-/home/jkeim/projects/groundwords/wordcartel-core/target/debug/deps/libsmallvec-93658ed5814bd510.rmeta: /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.2/src/lib.rs
-
-/home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.2/src/lib.rs:
--- wordcartel-core/target/debug/deps/unicode_width-e710e4632aa24d43.d
+++
@@ -1,8 +0,0 @@
-/home/jkeim/projects/groundwords/wordcartel-core/target/debug/deps/unicode_width-e710e4632aa24d43.d: /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-width-0.1.14/src/lib.rs /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-width-0.1.14/src/tables.rs
-
-/home/jkeim/projects/groundwords/wordcartel-core/target/debug/deps/libunicode_width-e710e4632aa24d43.rlib: /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-width-0.1.14/src/lib.rs /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-width-0.1.14/src/tables.rs
-
-/home/jkeim/projects/groundwords/wordcartel-core/target/debug/deps/libunicode_width-e710e4632aa24d43.rmeta: /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-width-0.1.14/src/lib.rs /home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-width-0.1.14/src/tables.rs
-
-/home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-width-0.1.14/src/lib.rs:
-/home/jkeim/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-width-0.1.14/src/tables.rs:
